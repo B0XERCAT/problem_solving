e37cc6f75aa55da82fe9ed0986214ebfeed65237
Kwon Seojin <97675977+seojin3154@users.noreply.github.com>
2023-10-04 00:20:57 +0900

    [Gold IV] Title: 스도쿠, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int table[9][9];
+bool rowCheck[9][9];
+bool colCheck[9][9];
+bool boxCheck[9][9];
+bool foundSolution = false;
+
+void solve(int index)
+{
+    if (index == 81)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+                cout << table[i][j] << " ";
+            cout << "\n";
+        }
+        foundSolution = true;
+    }
+
+    int x = index / 9;
+    int y = index % 9;
+
+    if (foundSolution)
+        return;
+
+    if (table[x][y] == 0)
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            if (!rowCheck[x][i] && !colCheck[y][i] && !boxCheck[(x / 3) * 3 + (y / 3)][i])
+            {
+                rowCheck[x][i] = true;
+                colCheck[y][i] = true;
+                boxCheck[(x / 3) * 3 + (y / 3)][i] = true;
+                table[x][y] = i;
+                solve(index + 1);
+                rowCheck[x][i] = false;
+                colCheck[y][i] = false;
+                boxCheck[(x / 3) * 3 + (y / 3)][i] = false;
+                table[x][y] = 0;
+            }
+        }
+    }
+    else
+        solve(index + 1);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
+        {
+            cin >> table[i][j];
+            if (table[i][j] != 0)
+            {
+                rowCheck[i][table[i][j]] = true;
+                colCheck[j][table[i][j]] = true;
+                boxCheck[(i / 3) * 3 + (j / 3)][table[i][j]] = true;
+            }
+        }
+
+    solve(0);
+
+    return 0;
+}

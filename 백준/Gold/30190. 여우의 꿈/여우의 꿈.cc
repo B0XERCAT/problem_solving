6616e58e4c813214c561a9177218dc4124adb6de
Kwon Seojin <97675977+seojin3154@users.noreply.github.com>
2023-10-09 13:34:29 +0900

    Update 여우의 꿈.cc

--- 백준/Gold/30190. 여우의 꿈/여우의 꿈.cc
+++ 백준/Gold/30190. 여우의 꿈/여우의 꿈.cc
@@ -1,10 +1,5 @@
 #include <iostream>
-#include <vector>
-#include <cstring>
-#include <string>
-#include <queue>
-#include <algorithm>
-#include <cmath>
+
 #define ll long long
 #define MAX 1000002
 #define MOD 1000000007
@@ -12,28 +7,8 @@
 using namespace std;
 
 ll res = 0;
-ll dp[MAX];
+ll power2[MAX];
 int goal[MAX], N, K;
-
-ll recPower(ll a, ll x)
-{
-    if (x == 0)
-        return 1;
-    if (x == 1)
-        return a;
-    if (dp[x] != -1)
-        return dp[x];
-
-    ll half = recPower(a, x / 2) % MOD;
-
-    if (x % 2 == 0)
-        dp[x] = (half * half) % MOD;
-
-    else
-        dp[x] = (((half * half) % MOD) * a) % MOD;
-
-    return dp[x];
-}
 
 void hanoi(int from, int to, int height)
 {
@@ -44,7 +19,7 @@ void hanoi(int from, int to, int height)
     }
     if (from != to)
     {
-        res += recPower(2, height - 1);
+        res += power2[height - 1];
         res %= MOD;
         hanoi(6 - (from + to), goal[height - 1], height - 1);
     }
@@ -58,11 +33,16 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    memset(dp, -1, sizeof(dp));
+    power2[0] = 1;
+    power2[1] = 2;
+
     cin >> N >> K;
 
     for (int i = 1; i <= N; i++)
+    {
         cin >> goal[i];
+        power2[i + 1] = power2[i] * 2 % MOD;
+    }
 
     hanoi(K, goal[N], N);
 

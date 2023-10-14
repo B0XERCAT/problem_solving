c3bebbb8a6f7dd8ca17c3741a4a55a0056a47f66
Kwon Seojin <97675977+seojin3154@users.noreply.github.com>
2023-10-14 12:02:34 +0900

    [Gold I] Title: 최솟값, Time: 60 ms, Memory: 6308 KB -BaekjoonHub

---
+++ 백준/Gold/10868. 최솟값/최솟값.cc
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#define ll long long
+#define MAX 1000000001
+using namespace std;
+
+vector<pair<ll, ll>> tree;
+
+void update(int index, ll val)
+{
+    tree[index] = {val, val};
+
+    while (index > 1)
+    {
+        index /= 2;
+        tree[index / 2].first = min(tree[index / 2].first, tree[index].first);
+        tree[index / 2].second = max(tree[index / 2].second, tree[index].second);
+    }
+}
+
+void init(int i)
+{
+    while (i != 1)
+    {
+        tree[i / 2].first = min(tree[i / 2].first, tree[i].first);
+        tree[i / 2].second = max(tree[i / 2].second, tree[i].second);
+        i--;
+    }
+}
+
+pair<ll, ll> minAndMax(int s, int e)
+{
+    ll partMin = MAX;
+    ll partMax = 0;
+
+    while (s <= e)
+    {
+        if (s % 2 == 1)
+        {
+            partMin = min(partMin, tree[s].first);
+            partMax = max(partMax, tree[s].second);
+            s++;
+        }
+        if (e % 2 == 0)
+        {
+            partMin = min(partMin, tree[e].first);
+            partMax = max(partMax, tree[e].second);
+            e--;
+        }
+        s /= 2;
+        e /= 2;
+    }
+    return {partMin, partMax};
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    int treeHeight = 0;
+    int length = N;
+
+    while (length != 0)
+    {
+        length /= 2;
+        treeHeight++;
+    }
+
+    int treeSize = int(pow(2, treeHeight + 1));
+    int leafStartIdx = treeSize / 2;
+
+    tree.resize(treeSize + 1, {MAX, 0});
+
+    for (int i = leafStartIdx; i < leafStartIdx + N; i++)
+    {
+        ll x;
+        cin >> x;
+        tree[i] = {x, x};
+    }
+
+    init(treeSize - 1);
+
+    for (int i = 0; i < M; i++)
+    {
+        ll s, e;
+        cin >> s >> e;
+
+        s = s + leafStartIdx - 1;
+        e = e + leafStartIdx - 1;
+        cout << minAndMax(s, e).first << "\n";
+    }
+
+    return 0;
+}

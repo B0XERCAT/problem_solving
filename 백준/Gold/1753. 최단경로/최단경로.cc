865a5786e538d17408ca92ea608f8e2b70a53826
Kwon Seojin <97675977+seojin3154@users.noreply.github.com>
2023-10-26 18:04:53 +0900

    [Gold IV] Title: 최단경로, Time: 100 ms, Memory: 9188 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define INF 987654321
+#define MAX 20001
+#define pii pair<int, int>
+using namespace std;
+
+vector<pair<int, int>> graph[MAX];
+vector<int> dist(MAX);
+vector<bool> visited(MAX);
+int V, E, start;
+
+void dijkstra(int start)
+{
+    dist[start] = 0;
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    pq.push({0, start});
+
+    while (!pq.empty())
+    {
+        int cur = pq.top().second;
+        int distance = pq.top().first;
+        pq.pop();
+
+        if (visited[cur])
+            continue;
+
+        visited[cur] = true;
+        
+        for (auto pair : graph[cur])
+        {
+            int next = pair.first;
+            int nextDistance = distance + pair.second;
+            if (nextDistance < dist[next])
+            {
+                dist[next] = nextDistance;
+                pq.push({nextDistance, next});
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> V >> E >> start;
+
+    for (int i = 0; i < E; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph[u].push_back({v, w});
+    }
+
+    for (int i = 1; i <= V; i++)
+        dist[i] = INF;
+
+    dijkstra(start);
+
+    for (int i = 1; i <= V; i++)
+    {
+        if (dist[i] == INF)
+        {
+            cout << "INF\n";
+            continue;
+        }
+        cout << dist[i] << "\n";
+    }
+
+    return 0;
+}

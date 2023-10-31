a409fad4101396ad4d3b254abf1fe100da6fec1a
Kwon Seojin <97675977+seojin3154@users.noreply.github.com>
2023-10-31 14:35:33 +0900

    [Platinum V] Title: Strongly Connected Component, Time: 24 ms, Memory: 3544 KB -BaekjoonHub

---
+++ 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#define MAX 10001
+using namespace std;
+
+int idx = 0, id[MAX];
+bool finished[MAX];
+vector<int> graph[MAX];
+vector<vector<int>> SCC;
+stack<int> s;
+
+int dfs(int x)
+{
+  id[x] = ++idx;
+  s.push(x);
+
+  int result = id[x];
+
+  for (auto next : graph[x])
+  {
+    if (id[next] == 0)
+      result = min(result, dfs(next));
+    else if (!finished[next])
+      result = min(result, id[next]);
+  }
+
+  if (result == id[x])
+  {
+    vector<int> scc;
+    while (1)
+    {
+      int t = s.top();
+      s.pop();
+      scc.push_back(t);
+      finished[t] = true;
+      if (t == x)
+        break;
+    }
+    sort(scc.begin(), scc.end());
+    SCC.push_back(scc);
+  }
+
+  return result;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int V, E;
+  cin >> V >> E;
+
+  for (int i = 0; i < E; i++)
+  {
+    int A, B;
+    cin >> A >> B;
+    graph[A].push_back(B);
+  }
+
+  for (int i = 1; i <= V; i++)
+    if (id[i] == 0)
+      dfs(i);
+
+  sort(SCC.begin(), SCC.end());
+
+  cout << SCC.size() << "\n";
+
+  for (auto scc : SCC)
+  {
+    for (auto node : scc)
+      cout << node << " ";
+    cout << "-1\n";
+  }
+
+  return 0;
+}

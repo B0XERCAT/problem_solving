61c08ea6e38d981d501098b36855e21da8b33788
Kwon Seojin <97675977+seojin3154@users.noreply.github.com>
2023-11-13 10:16:51 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll N, M;
+    cin >> N >> M;
+    cout << N * M / 2;
+
+    return 0;
+}

777f1828f01b143aa7ef3f5d08522706f15563f2
Kwon Seo Jin <97675977+B0XERCAT@users.noreply.github.com>
2023-12-08 17:48:19 +0900

    [Bronze III] Title: TGN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    while (N--)
+    {
+        int r, e, c;
+        cin >> r >> e >> c;
+        if (r < e - c)
+            cout << "advertise\n";
+        else if (r == e - c)
+            cout << "does not matter\n";
+        else
+            cout << "do not advertise\n";
+    }
+
+    return 0;
+}
